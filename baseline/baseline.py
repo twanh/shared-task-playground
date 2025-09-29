42b69c574c4ea47994c1ddc1278898011c56c299
Twan Huiskens <huiskenstwan@gmail.com>
2025-09-29 14:08:43 +0200

    Add basic prompting

--- baseline/baseline.py
+++ baseline/baseline.py
@@ -16,12 +16,17 @@ def create_argparser() -> argparse.Namespace:
     )
 
 
-    parser.add_argument('--prompt-name', help="The name (of the file) of the prompt to use (inside prompts dir)", default="prompt1")
+    parser.add_argument('--system-prompt', help="The name of the system prompt to use")
+    parser.add_argument('--prompt', help="The name (of the file) of the prompt to use (inside prompts dir)", default="prompt1")
 
     return parser.parse_args()
 
 
-def create_prompt(prompt_name: str, prompts_dir: str = PROMPTS_DIR, **kwargs) -> str:
+def create_prompt(
+    prompt_name: str,
+    prompts_dir: str = PROMPTS_DIR,
+    **kwargs
+) -> str:
 
     if not prompt_name.endswith('.prompt'):
         prompt_name = prompt_name + '.prompt'
@@ -30,19 +35,47 @@ def create_prompt(prompt_name: str, prompts_dir: str = PROMPTS_DIR, **kwargs) ->
     with open(prompt_path, 'r') as prompt_file:
         prompt = prompt_file.read()
 
-    return prompt.format(**kwargs)
+    try:
+        formated_prompt = prompt.format(**kwargs)
+    except KeyError as e:
+        print(e)
+        # Do not format the prompt but return the orignal tempalte
+        formated_prompt = prompt
+    return formated_prompt
 
+
+def create_chat_template(
+    system_prompt: str|None,
+    user_prompt: str|None,
+    history: list|None = None
+) -> list[dict[str, str]]:
+
+    conv: list[dict[str,str]] = []
+
+    if system_prompt is not None:
+        conv.append({"system": system_prompt})
+
+    if user_prompt is not None:
+        conv.append({"user": user_prompt})
+
+    if history is not None:
+        return history.append(conv)
+
+    return conv
 
 
 def main():
 
     args = create_argparser()
 
-    prompt = create_prompt(args.prompt_name)
 
-    llm = LLM(model=args.model)
+    system_prompt = None
+    if args.system_prompt:
+        system_prompt = create_prompt(args.system_prompt)
+    user_prompt = create_prompt(args.prompt, syllogism="test syllo")
 
-
+    chat = create_chat_template(system_prompt, user_prompt)
+    print(chat)
 
 
 if __name__ == "__main__":
